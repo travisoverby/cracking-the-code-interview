f6308e71dbd20081acef1a910640f926570e56de
Travis Overby <travisoverby@gmail.com>
2018-02-04 13:21:46 -0800

    1.1-IsUnique Test more input; remove static inner class

--- 1.1-IsUnique/Solution.java
+++ 1.1-IsUnique/Solution.java
@@ -3,32 +3,31 @@ import java.util.*;
 public class Solution {
 
   public static void main(String[] args) {
-    IsUnique.test("abcdefghijklmnopqrstuvwxyz", true);   
+    test("abcdefghijklmnopqrstuvwxyz", true);
+    test("11111", false);
+    test("12121212", true);
   }
 
-  private static class IsUnique {
+  
+  public static void test(String input, boolean expectedResult) {
+    boolean actualResult = isUnique(input);
     
-    public static void test(String input, boolean expectedResult) {
-      boolean actualResult = isUnique(input);
-      
-      System.out.println("Input           : " + input);
-      System.out.println("Expected result : " + expectedResult);
-      System.out.println("Actual result   : " + actualResult);    
-      System.out.println();
-    }
+    System.out.println("Input           : " + input);
+    System.out.println("Expected result : " + expectedResult);
+    System.out.println("Actual result   : " + actualResult);    
+    System.out.println();
+  }
 
-    private static boolean isUnique(String input) {
-      Set<Character> chars = new HashSet<Character>();
-      for (char c : input.toCharArray()) {
-        if (chars.contains(c)) {
-          return false;
-        } else {
-          chars.add(c);
-        }
-      } 
-     return true;
-    }
-
+  private static boolean isUnique(String input) {
+    Set<Character> chars = new HashSet<Character>();
+    for (char c : input.toCharArray()) {
+      if (chars.contains(c)) {
+        return false;
+      } else {
+        chars.add(c);
+      }
+    } 
+   return true;
   }
 
 }
